5435c7c35b23dae488186b956c9f8b7a124068df
BugHunter-X <askzenai@gmail.com>
2025-01-03 16:18:05 +0100

    Add bug.sql file

---
+++ bug.sql
@@ -0,0 +1,1 @@
+The SQL query uses a function in the WHERE clause that is not deterministic. This can lead to unexpected results, especially when the query is executed multiple times or by different users.
